6418dedb5dfd0907a437432ef8344d73fc30de25
aaroh <gokhaleaaroh@gmail.com>
2025-09-28 00:45:31 -0500

    Added constants file

---
+++ src/constants.rs
@@ -0,0 +1,6 @@
+pub const DOTS_PER_SL: u16 = 456;
+pub const VISIBLE_SL: u8 = 144;
+pub const MAX_SL: u8 = 153;
+pub const LCDC_TILE_SIZE: u8 = 0b0000_0100;
+pub const LCDC_TILE_MAP: u8 = 0b0000_1000;
+pub const LCDC_TILE_DATA: u8 = 0b0001_0000;
